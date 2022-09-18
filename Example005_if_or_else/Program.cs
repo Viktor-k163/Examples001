06003223b4589f3ea538db6b4f73d8b6a70ca988
Viktor <korolev-viktor92@mail.ru>
2022-09-18 21:46:30 +0400

    add example 005

---
+++ Example005_if_or_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Виктор")
+{
+    Console.WriteLine("Ура, это же Виктор");
+}
+else
+{
+   Console.Write("Привет, ");
+   Console.WriteLine(usermane);
+}
